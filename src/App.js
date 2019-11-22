861f9cb11c7206b3e16f8d05fd0385a61299420f
glukor <clement.fra@hotmail.fr>
2019-11-22 16:37:26 +0100

    Creating and checking the rendering of TodoList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import TodoList from './TodoList';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      
+    <div >
+      <TodoList />
     </div>
   );
 }
---
+++ src/TodoList.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+
+class TodoList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { todos: []}
+
+    }
+    render() {
+        return(
+            <div>
+                <h1>Todo List</h1>
+                {/* TODO liste pour placer les items */}
+                <ul>
+                    <li>Todo 1</li>
+                    <li>Todo 2</li>
+                    <li>Todo 3</li>
+                </ul>
+
+            </div>
+        );
+    }
+}
+
+export default TodoList;
