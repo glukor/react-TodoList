e663b194bcdb1aa0b8cb63f80be098be657a8a9c
glukor <clement.fra@hotmail.fr>
2019-11-22 19:10:47 +0100

    Removing Todos

--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
 
 class Todo extends Component {
+    constructor(props){
+        super(props);
+        this.handleRemove = this.handleRemove.bind(this);
+    }
+
+    handleRemove() {
+        this.props.removeTodo(this.props.id);
+    }
+
     render() {
         return (
             <div>
                 {/* TODO Modifier la tâche */}
                 <button>Edit</button>
                 {/* TODO Supprimer la tâche */}
-                <button>X</button>
-                {/* TODO Tâche à render */}
+                <button onClick={this.handleRemove}>X</button>
+
                 <li>{this.props.task}</li>
             </div>
         )
--- src/TodoList.js
+++ src/TodoList.js
@@ -7,9 +7,9 @@ class TodoList extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { todos: [{ task: 'Arroser les plantes' }, { task: 'Faire les courses' }] };
+        this.state = { todos: [] };
         this.create = this.create.bind(this);
-
+        this.remove = this.remove.bind(this);
     }
 
     create(newTodo) {
@@ -18,9 +18,20 @@ class TodoList extends Component {
         });
     }
 
+    remove(id) {
+        this.setState({
+            todos: this.state.todos.filter(t => t.id !== id)
+        })
+    }
+
     render() {
         const todos = this.state.todos.map(todo => {
-            return <Todo task={todo.task} />;
+            return <Todo
+                key={todo.id}
+                id={todo.id}
+                task={todo.task}
+                removeTodo={this.remove}
+            />;
         });
         return (
             <div>
